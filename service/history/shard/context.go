ebf08c79202ccd622e646cfd606a6aec05ee0e80
Gaziza Yestemirova <gaziza@uber.com>
2025-07-18 10:56:25 +0200

    [domain-deletion]Drop graceful failover markers when domain is deprecated

--- service/history/shard/context.go
+++ service/history/shard/context.go
@@ -1471,6 +1471,16 @@ func (s *contextImpl) ValidateAndUpdateFailoverMarkers() ([]*types.FailoverMarke
 			s.RUnlock()
 			return nil, err
 		}
+
+		// Check if the domain is deprecated
+		if domainEntry.GetInfo() != nil && domainEntry.GetInfo().Status == persistence.DomainStatusDeprecated {
+			s.logger.Info("Domain is deprecated marking failover marker for cleanup",
+				tag.WorkflowDomainName(domainEntry.GetInfo().Name),
+				tag.WorkflowDomainID(marker.GetDomainID()))
+			completedFailoverMarkers[marker] = struct{}{}
+			continue
+		}
+
 		isActive := domainEntry.IsActiveIn(s.GetClusterMetadata().GetCurrentClusterName())
 		if isActive || domainEntry.GetFailoverVersion() > marker.GetFailoverVersion() {
 			completedFailoverMarkers[marker] = struct{}{}
